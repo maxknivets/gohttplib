27ce02f7422ac20d9ba316f41b4a0aed34ce013b
Max Knivets <knivecmaksim@gmail.com>
2019-07-28 00:49:47 +0300

    Added a shutdown feature

--- gohttplib.go
+++ gohttplib.go
@@ -23,14 +23,25 @@ import (
 	"bytes"
 	"net/http"
 	"unsafe"
+	"context"
 )
 
 var cpointers = PtrProxy()
+var srv http.Server = http.Server
 
 //export ListenAndServe
 func ListenAndServe(caddr *C.char) {
 	addr := C.GoString(caddr)
-	http.ListenAndServe(addr, nil)
+	srv.Addr = addr
+	Shutdown()
+	srv.ListenAndServe()
+}
+
+// export Shutdown
+func Shutdown() {
+	http.HandleFunc("/shutdown", func(w http.ResponseWriter, r *http.Request) {
+		srv.Shutdown(context.Background())
+	})
 }
 
 //export HandleFunc
